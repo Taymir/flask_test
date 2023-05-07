3b68b5aea8b9758f832404224d1ffb8965bd63f3
Tim <taymir@gmail.com>
2023-05-07 21:50:50 +0300

    added tests and changed github actions workflow

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -9,4 +8,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
