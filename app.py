346686c6fa829eda1d4e77b5b73411d70c58c527
Tim <taymir@gmail.com>
2023-05-08 11:19:17 +0300

    updated workflow

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Тест!'
-# just a comment
+# just a comment edited
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
